185978150964257fe7b231a51fb935817d5458e2
Alex Wade <wade.alex.c@gmail.com>
2020-11-19 16:50:27 -0500

    Cleanup! - We now filter out our own peer ID and localhost from any results - Moved writeToFile before closing our datastore, since that seems to panic sometimes... - Added a CLI flag -d <uint>, which allows user to specify crawl duration

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"context"
 	"encoding/csv"
-	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
 	"os/signal"
@@ -14,62 +14,61 @@ import (
 	"github.com/ipfs/go-datastore"
 	badger "github.com/ipfs/go-ds-badger"
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/routing"
 	"github.com/libp2p/go-libp2p-core/test"
 	host "github.com/libp2p/go-libp2p-host"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	peerstore "github.com/libp2p/go-libp2p-peerstore"
 	"github.com/libp2p/go-libp2p/p2p/protocol/identify"
-	"github.com/multiformats/go-multiaddr"
 )
 
-type logOutput struct {
-	Id       string
-	Agent    string
-	Protocol string
-	Addrs    []string
-	Protos   []string
-}
+// Workers are distinct goroutines used for querying the DHT
+const Workers = 8
 
-const WORKERS = 8
+// PeerIDFile stores IDs of discovered peers, along with the ID
+// of the peer who responded to our query. Records take the form:
+// pOrigin,pResponse
+// ... where each field is a peer ID
+const PeerIDFile = "peerIDs.csv"
 
-const PEERID_FILE = "peerIDs.csv"
-const ID_TO_IP_FILE = "peerIPs.csv"
+// IDToIPFile stores IPv4/IPv6/DNS4/DNS6 addresses of peers, along
+// with the ID of the peer located at that address. Records take the form:
+// pID,ip:port
+const IDToIPFile = "peerIPs.csv"
 
+var (
+	duration = flag.Uint("d", 0, "(Optional) Number of minutes to crawl. No limit by default.")
+)
+
+// Crawler holds the information about an active crawl
 type Crawler struct {
-	host host.Host // Our libp2p node
+	// IPFS / libp2p fields
+	host host.Host
 	dht  *dht.IpfsDHT
 	ds   datastore.Batching
 	id   *identify.IDService
 
-	results map[peer.ID][]*peer.AddrInfo
-	// Writer/File for peerIDs discovered
-	idWriter *csv.Writer
-	idFile   *os.File
-	// Writer/File for peerID -> IP mapping
-	ipWriter *csv.Writer
-	ipFile   *os.File
+	// Result aggregator and file/writer pairs for output
+	results      map[peer.ID][]*peer.AddrInfo
+	peerIDWriter *csv.Writer
+	peerIDFile   *os.File
+	IDToIPWriter *csv.Writer
+	IDToIPFile   *os.File
 
+	// Context and cancel function passed to DHT queries
 	ctx    context.Context
 	cancel context.CancelFunc
-	events <-chan *routing.QueryEvent // DHT sends QueryEvents to this channel
-	// Waits for a collection of goroutines to finish
-	// Main goroutine (this file) calls wg.Add to set the number of goroutines to wait for
-	// Then, each goroutine runs and calls Done when finished
+	// We receive QueryEvents from the DHT on this channel
+	events <-chan *routing.QueryEvent
+
 	wg sync.WaitGroup
 }
 
-// Alias Crawler as notifee, which will implement network.Notifiee interface
-type notifee Crawler
-
-// Declare that alias'd Crawler implements network.Notifiee
-var _ network.Notifiee = (*notifee)(nil)
-
+// NewCrawler creates an IPFS crawler.
+// It also opens CSV files for output and starts our libp2p node.
 func NewCrawler() *Crawler {
-	ctx, cancel := context.WithCancel(context.Background())
 	// Create a context that, when passed to DHT queries, emits QueryEvents to this channel
+	ctx, cancel := context.WithCancel(context.Background())
 	ctx, events := routing.RegisterForQueryEvents(ctx)
 
 	c := &Crawler{
@@ -84,29 +83,30 @@ func NewCrawler() *Crawler {
 	return c
 }
 
+// Create output files, as well as writers for those files
 func (c *Crawler) initCSV() {
-	file, err := os.Create(PEERID_FILE)
+	file, err := os.Create(PeerIDFile)
 	if err != nil {
 		panic(err)
 	}
 	writer := csv.NewWriter(file)
 
-	c.idWriter = writer
-	c.idFile = file
+	c.peerIDWriter = writer
+	c.peerIDFile = file
 
-	file, err = os.Create(ID_TO_IP_FILE)
+	file, err = os.Create(IDToIPFile)
 	if err != nil {
 		panic(err)
 	}
 	writer = csv.NewWriter(file)
 
-	c.ipWriter = writer
-	c.ipFile = file
+	c.IDToIPWriter = writer
+	c.IDToIPFile = file
 }
 
+// Start our libp2p node
 func (c *Crawler) initHost() {
 	var err error
-	// Create a new libp2p node
 	c.host, err = libp2p.New(context.Background())
 	if err != nil {
 		panic(err)
@@ -121,9 +121,8 @@ func (c *Crawler) initHost() {
 	// Create DHT client
 	c.dht = dht.NewDHTClient(c.ctx, c.host, c.ds)
 
-	// Iterate over default bootstrap peers
+	// Bootstrap from bootstrap peers
 	for _, a := range dht.DefaultBootstrapPeers {
-		// Convert multiaddr to AddrInfo
 		pi, err := peer.AddrInfoFromP2pAddr(a)
 		if err != nil {
 			panic(err)
@@ -141,21 +140,14 @@ func (c *Crawler) initHost() {
 	c.id = identify.NewIDService(c.host)
 }
 
-func (c *Crawler) close() {
-	c.cancel()
-	c.wg.Done()
+// Spawn workers to begin querying DHT
+// Spawn an aggregator to collect results from c.events
+// Spawn a reporter to print incremental status updates
+func (c *Crawler) start(numWorkers int) {
+	fmt.Println("IPBW: Starting crawl")
+	fmt.Printf("Spawning %d workers\n", numWorkers)
 
-	if err := c.ds.Close(); err != nil {
-		fmt.Printf("error while shutting down: %v\n", err)
-	}
-
-	c.writeToFile()
-}
-
-func (c *Crawler) start() {
-	fmt.Println("INTERPLANETARY BLACK WIDOW HAS BEGUN CRAWLING...")
-
-	for i := 0; i < WORKERS; i++ {
+	for i := 0; i < numWorkers; i++ {
 		c.wg.Add(1)
 		go c.worker()
 	}
@@ -163,11 +155,13 @@ func (c *Crawler) start() {
 	go c.aggregator()
 	go c.reporter()
 
-	c.host.Network().Notify((*notifee)(c))
+	// c.host.Network().Notify((*notifee)(c))
 }
 
+// Each worker generates a random peer ID, then queries the DHT
+// Workers time out after 10 seconds and generate a new ID to query
+// Results are received via the c.events channel
 func (c *Crawler) worker() {
-	fmt.Println("Starting glorious worker...")
 Work:
 	// Get a random peer ID
 	id, err := test.RandPeerID()
@@ -180,35 +174,52 @@ Work:
 	goto Work
 }
 
+// Incrementally prints status updates
 func (c *Crawler) reporter() {
-	fmt.Println("Starting reporter...")
-
+	var elapsed int = 0
 	for {
 		select {
 		case <-time.After(10 * time.Second):
-			fmt.Printf("--- %d unique peers so far\n", len(c.results))
+			elapsed += 10
+			fmt.Printf("--- %d unique peers", len(c.results))
+			if elapsed == 60 {
+				// This is very important
+				fmt.Printf("; time elapsed: %d minute\n", elapsed/60)
+			} else if elapsed%60 == 0 {
+				fmt.Printf("; time elapsed: %d minutes\n", elapsed/60)
+			} else {
+				fmt.Printf("\n")
+			}
 		case <-c.ctx.Done():
 			return
 		}
 	}
 }
 
+// Receives and filters QueryEvents from the DHT
 func (c *Crawler) aggregator() {
-	fmt.Println("Starting aggregator...")
-
 	for e := range c.events {
 		// We only care about PeerResponse events, as these
 		// are peers informing us of peers they know.
 		if e.Type == routing.PeerResponse {
+			// Make sure we don't record responses from our own node!
+			self := c.dht.PeerID()
+			if self.String() == e.ID.String() {
+				fmt.Printf("Self-report; skipping!\n")
+				continue
+			}
+			// Filter out e.Responses - we don't want our own node
+			queryResponses := filterSelf(self, e.Responses)
+			// No remaining responses? Skip.
+			if len(queryResponses) == 0 {
+				continue
+			}
 
 			// If we haven't seen this peer yet, we don't need to think too hard
 			// Just set the responses and continue!
 			if _, exists := c.results[e.ID]; !exists {
-				c.results[e.ID] = e.Responses
-				// fmt.Printf("New peer %s! Total unique peers: %d\n", e.ID.Pretty(), len(results))
+				c.results[e.ID] = queryResponses
 			} else {
-				// count := len(results[e.ID])
-
 				// Okay, we've seen this peer already!
 				// Add new responses and filter out duplicates:
 				seen := map[peer.ID]struct{}{}
@@ -225,7 +236,7 @@ func (c *Crawler) aggregator() {
 				}
 
 				// Iterate over incoming results and append IDs we haven't seen:
-				for _, r := range e.Responses {
+				for _, r := range queryResponses {
 					// filter dupes
 					if _, exists := seen[r.ID]; exists {
 						continue
@@ -235,35 +246,41 @@ func (c *Crawler) aggregator() {
 					responses = append(responses, r)
 				}
 
-				c.results[e.ID] = responses
+				// Skip empty responses
+				if len(responses) == 0 {
+					continue
+				}
 
-				// newCount := len(results[e.ID])
-				// fmt.Printf("Existing peer %s! Added %d new conns for a total of %d\n", e.ID.Pretty(), newCount-count, newCount)
+				c.results[e.ID] = responses
 			}
 		}
 	}
 }
 
-// in go:
-// peer: [peerA, peerB]
+// Called when we finish a crawl. Kills our goroutines and writes output to a file.
+func (c *Crawler) close() {
+	c.cancel()
+	c.wg.Done()
 
-// f1 list tuple(ID,ID)
-// pOrigina,pDestx
-// pOriginb,pDesty
+	c.writeToFile()
 
-// f2 map ID -> IP
-// pID,pIP
+	if err := c.ds.Close(); err != nil {
+		fmt.Printf("error while shutting down: %v\n", err)
+	}
+}
+
 func (c *Crawler) writeToFile() {
 	fmt.Println("Writing results to file...")
 
 	// Make sure we close our files and flush our writers at the end of this method
-	defer c.idFile.Close()
-	defer c.ipFile.Close()
+	defer c.peerIDFile.Close()
+	defer c.IDToIPFile.Close()
 
-	defer c.idWriter.Flush()
-	defer c.ipWriter.Flush()
+	defer c.peerIDWriter.Flush()
+	defer c.IDToIPWriter.Flush()
 
 	pStore := c.host.Peerstore()
+	var numSkipped int = 0
 
 	for pID, addrs := range c.results {
 		// Try to get IPv4/IPv6 addresses for this peer
@@ -271,7 +288,7 @@ func (c *Crawler) writeToFile() {
 
 		// Maybe we don't know ANY multiaddrs for this peer? Skip if so...
 		if len(pInfo) == 0 {
-			fmt.Printf("No addrs returned for peer %s; skipping...\n", pID.Pretty())
+			numSkipped++
 			continue
 		}
 
@@ -286,13 +303,16 @@ func (c *Crawler) writeToFile() {
 				ipFound   bool
 			)
 			for i, field := range fields {
-				// Found IPv4/IPv4 or DNS; add to result
+				// Found IPv4/IPv6 or DNS; add to result
 				if field == "ip4" || field == "ip6" || field == "dns4" || field == "dns6" {
 					if res != "" {
 						fmt.Printf("Possible malformed ip %s; skipping...\n", a.String())
 						break
 					} else if i+1 >= len(fields) {
 						fmt.Printf("Unexpected EOField %s; skipping...\n", a.String())
+						break
+					} else if fields[i+1] == "127.0.0.1" {
+						// Skip localhost
 						break
 					}
 					res = fields[i+1]
@@ -317,13 +337,13 @@ func (c *Crawler) writeToFile() {
 				result := make([]string, 2)
 				result[0] = pID.Pretty()
 				result[1] = res
-				c.ipWriter.Write(result)
+				c.IDToIPWriter.Write(result)
 			}
 		}
 
 		// So, we found multiaddrs for this peer, but didn't find IPv4 / IPv6? Weird... skip.
 		if count == 0 {
-			fmt.Printf("%s has %d maddrs, but no ip4/ip6!\nAddrs:%v\n", pID.Pretty(), len(pInfo), pInfo)
+			fmt.Printf("%s has %d maddrs, but no ip4/ip6/dns4/dns6!\nAddrs:%v\n", pID.Pretty(), len(pInfo), pInfo)
 			continue
 		}
 
@@ -331,106 +351,60 @@ func (c *Crawler) writeToFile() {
 			result := make([]string, 2)
 			result[0] = pID.Pretty()
 			result[1] = addr.ID.Pretty()
-			c.idWriter.Write(result)
+			c.peerIDWriter.Write(result)
 		}
 	}
+
+	fmt.Printf("Finishing write. Skipped %d peers with no known multiaddrs\n", numSkipped)
 }
 
-func (n *notifee) Connected(net network.Network, conn network.Conn) {
-	p := conn.RemotePeer()
-	pstore := net.Peerstore()
-
-	go func() {
-		// Ask the IDService about the connection
-		n.id.IdentifyConn(conn)
-		<-n.id.IdentifyWait(conn)
-
-		// Get the protocols supported by the peer
-		protos, err := pstore.GetProtocols(p)
-		if err != nil {
-			panic(err)
+/**
+ * Filter DHT query responses that contain our own peer ID
+ */
+func filterSelf(self peer.ID, responsesRaw []*peer.AddrInfo) []*peer.AddrInfo {
+	var res []*peer.AddrInfo = make([]*peer.AddrInfo, 0)
+	for _, info := range responsesRaw {
+		if info.ID.String() != self.String() {
+			res = append(res, info)
 		}
-
-		// Get all known / valid addresses for the peer
-		addrs := pstore.Addrs(p)
-
-		// Get peer's agent version
-		agent, err := pstore.Get(p, "AgentVersion")
-		switch err {
-		case nil:
-		case peerstore.ErrNotFound:
-			agent = ""
-		default:
-			panic(err)
-		}
-
-		// Get peer's protocol version
-		protocol, err := pstore.Get(p, "ProtocolVersion")
-		switch err {
-		case nil:
-		case peerstore.ErrNotFound:
-			protocol = ""
-		default:
-			panic(err)
-		}
-
-		line := logOutput{
-			Id:       p.Pretty(),
-			Agent:    agent.(string),
-			Protocol: protocol.(string),
-			Addrs: func() (ret []string) {
-				for _, a := range addrs {
-					ret = append(ret, a.String())
-				}
-				return ret
-			}(),
-			Protos: protos,
-		}
-
-		_, err = json.Marshal(line)
-		if err != nil {
-			panic(err)
-		}
-	}()
-}
-
-func (*notifee) Disconnected(_ network.Network, conn network.Conn) {
-	// fmt.Printf("Disconnected from peer: %s\n", conn.RemotePeer())
-}
-
-func (*notifee) Listen(_ network.Network, maddr multiaddr.Multiaddr) {
-	// fmt.Printf("Started listening on address: %s\n", maddr.String())
-}
-
-func (*notifee) ListenClose(_ network.Network, maddr multiaddr.Multiaddr) {
-	// fmt.Printf("Stopped listening on address: %s\n", maddr.String())
-}
-
-func (*notifee) OpenedStream(_ network.Network, st network.Stream) {
-	// fmt.Printf("Opened stream with ID: %s\n", st.ID())
-}
-
-func (*notifee) ClosedStream(_ network.Network, st network.Stream) {
-	// fmt.Printf("Closed stream with ID: %s\n", st.ID())
+	}
+	return res
 }
 
 func main() {
+
+	// Parse CLI flags
+	flag.Parse()
+
 	crawler := NewCrawler()
+	crawler.start(Workers)
 
-	crawler.start()
-
+	// Create a channel that will be notified of os.Interrupt or os.Kill signals
+	// This way, if we stop the crawler (e.g. with ^C), we can exit gracefully.
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt, os.Kill)
 
-	select {
-	case <-time.After(60 * time.Second):
-		fmt.Println("60 seconds reached; stopping crawler...")
-		crawler.close()
-		return
-	case <-ch:
-		crawler.close()
-		return
-	case <-crawler.ctx.Done():
-		return
+	if *duration != 0 {
+		// Duration specified - calculate total runtime
+		totalDuration := time.Duration(*duration) * 60 * time.Second
+		fmt.Printf("(Running %d minute crawl)\n", *duration)
+		select {
+		case <-time.After(totalDuration):
+			crawler.close()
+			return
+		case <-ch:
+			crawler.close()
+			return
+		case <-crawler.ctx.Done():
+			return
+		}
+	} else {
+		select {
+		case <-ch:
+			crawler.close()
+			return
+		case <-crawler.ctx.Done():
+			return
+		}
 	}
 }
